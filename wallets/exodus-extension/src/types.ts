d4dc5a75555b7b8153212ca0918cc17af8f7ed21
Kirill Bulgakov <kbulgakov@exodus.io>
2023-02-28 11:05:18 +0300

    chore: add config-like and types files

---
+++ wallets/exodus-extension/src/types.ts
@@ -0,0 +1,29 @@
+type Chain = string;
+
+export interface Account {
+  address: string;
+}
+
+interface ConnectionOptions {
+  chainId: Chain;
+}
+
+interface Transaction {
+  authInfo?: Uint8Array;
+  body?: Uint8Array;
+  chainId: Chain;
+  signer: string;
+}
+
+export interface ExodusCosmosProvider {
+  connect: (options: ConnectionOptions) => Promise<Account>;
+  signTransaction: (transaction: Transaction) => Promise<{ signature: string }>;
+}
+
+export interface Exodus {
+  cosmos: ExodusCosmosProvider;
+}
+
+export interface ExodusWindow {
+  exodus: Exodus;
+}
